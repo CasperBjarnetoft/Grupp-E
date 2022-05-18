da52d0346dd60c2a85cabe5375f8693a6de2168c
Yvette <nchombuayvta@gmil.com>
2022-05-18 11:24:34 +0200

    my orders

--- frontend/src/views/MyProfile.jsx
+++ frontend/src/views/MyProfile.jsx
@@ -9,7 +9,8 @@ const MyProfile = () => {
   const dispatch = useDispatch();
   const Navigate = useNavigate();
   const { orders, loading, error } = useSelector((state) => state.myOrders);
-  console.log(orders);
+  console.log("orders", orders);
+
   const { userInfo } = useSelector((state) => state.userLogin);
 
   useEffect(() => {
@@ -23,38 +24,32 @@ const MyProfile = () => {
   return (
     <>
       <p className="mt-3">
-        Welcome back <span class="text-info">{userInfo.name}! </span>{" "}
+        Welcome back{" "}
+        <span class="text-info">{userInfo.name ? userInfo.name : ""}! </span>{" "}
       </p>
 
-      {loading && loading}
-      {orders.length === 0 ? (
-        <p>There are no orders for this user</p>
-      ) : (
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Date</th>
-              <th scope="col">Total</th>
-              <th scope="col">Details</th>
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Date</th>
+            <th scope="col">Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <tr key={order._id}>
+              <td>{order._id}</td>
+              <td>{order.createdAt.substring(0, 10)}</td>
+              <td>
+                <Link to={`/order/${order._id}`}>
+                  <button>Details</button>
+                </Link>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => {
-              <tr key={order._id}>
-                <td>{order._id}</td>
-                <td>{order.createdAt.substring(0, 10)}</td>
-                <td>{order.totalPrice}</td>
-                <td>
-                  <Link to={`/order/${order._id}`}>
-                    <button>Details</button>
-                  </Link>
-                </td>
-              </tr>;
-            })}
-          </tbody>
-        </table>
-      )}
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
